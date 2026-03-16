8be76013303f71fdae8302d42cd3a8b9f655e898
TuanNgoc126 <buituanngoc.fi@gmail.com>
2026-03-16 16:10:57 +0200

    Commiting files after merge

--- First _File.go
+++ First _File.go
@@ -5,5 +5,8 @@ import "fmt"
 //This is a new file and our first commit
 
 //This is our code
+
 fmt.Println("I love Git")
 
+//This is a change to our code
+fmt.Println("I love merging and branching")
